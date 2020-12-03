60f91de1e6e4631d1114918c6615adbdfc4b6154
Lars-Olof Moilanen <lo@jpro.no>
2020-12-03 19:16:47 +0100

    day03

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::time::Instant;
 
 mod day01;
 mod day02;
+mod day03;
 
 pub mod expenses;
 pub mod passwords;
@@ -11,7 +12,9 @@ fn main() {
         || print!("day01 pt. 1: {:8}", day01::pt1(day01::input())),
         || print!("day01 pt. 2: {:8}", day01::pt2(day01::input())),
         || print!("day02 pt. 1: {:8}", day02::pt1(day02::input())),
-        || print!("day02 pt. 2: {:8}", day02::pt2(day02::input()))
+        || print!("day02 pt. 2: {:8}", day02::pt2(day02::input())),
+        || print!("day03 pt. 1: {:8}", day03::pt1(day03::input())),
+        || print!("day03 pt. 2: {:8}", day03::pt2(day03::input()))
     ];
     for x in problems {
         let start = Instant::now();
